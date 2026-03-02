f4258b7c4fd6c15dee694c8420d54e984414c961
Christian Coenen <christian300507@yahoo.com>
2026-03-02 14:34:34 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+
+import HomePage from './pages/HomePage'
+
+const App = () => {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path='/' element={<Navigate to='home' replace />} />
+                <Route path='/home' element={<HomePage />} />
+            </Routes>
+        </BrowserRouter>
+  )
+}
+
+export default App
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,9 @@
+const HomePage = () => {
+    return (
+        <div className="home-page">
+            The quick brown fox jumps over the lazy dog.
+        </div>
+    )
+}
+
+export default HomePage
