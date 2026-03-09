b15d613b3d292ee9d0bd3de73e37635d02a39f8d
Christian Coenen <christian300507@yahoo.com>
2026-03-09 12:40:22 +0100

    refactor(drawer-navigation): complete component with CSS and JSX
    - Implement full DrawerNavigation functionality.
    - Update styling to match portfolio layout.

--- src/components/features/navigation/DrawerNavigation.jsx
+++ src/components/features/navigation/DrawerNavigation.jsx
@@ -1,34 +1,95 @@
+import { useEffect } from 'react'
 import { NavLink } from 'react-router-dom'
 import { X } from 'lucide-react'
 import './DrawerNavigation.css'
 
 const DrawerNavigation = ({ isOpen, onClose }) => {
+    useEffect(() => {
+        if (isOpen) {
+            document.body.style.overflow = 'hidden'
+        } else {
+            document.body.style.overflow = ''
+        }
+
+        return () => {
+            document.body.style.overflow = ''
+        }
+    }, [isOpen])
+
     return (
         <div className={`drawer-navigation ${isOpen ? 'is-open' : ''}`}>
             <div className='drawer-navigation-overlay' onClick={onClose} />
 
             <div className='drawer-navigation-panel'>
-                <div>
-                    {/* TODO: add close button for drawer navigation */}
-                    <X className='close-' />
-
+                <div
+                    className='drawer-navigation-close-panel'
+                    
+                >
+                    <button
+                        type='button'
+                        className='drawer-navigation-close-button'
+                        onClick={onClose}
+                        aria-label='Close drawer navigation'
+                    >
+                        <X className='drawer-navigation-close-button-icon' />
+                    </button>
                 </div>
 
                 <ul className='drawer-navigation-list'>
                     <li className='drawer-navigation-link'>
-                        <NavLink to='/home' onClick={onClose}>Home</NavLink>
+                        <NavLink
+                            to='/home'
+                            className={({ isActive }) =>
+                                isActive
+                                    ? "drawer-navigation-link current"
+                                    : "drawer-navigation-link"
+                            }
+                            onClick={onClose}
+                        >
+                            Home
+                        </NavLink>
                     </li>
 
                     <li className='drawer-navigation-link'>
-                        <NavLink to='/art' onClick={onClose}>Art</NavLink>
+                        <NavLink
+                            to='/art'
+                            className={({ isActive }) =>
+                                isActive
+                                    ? "drawer-navigation-link current"
+                                    : "drawer-navigation-link"
+                            }
+                            onClick={onClose}
+                        >
+                            Art
+                        </NavLink>
                     </li>
 
                     <li className='drawer-navigation-link'>
-                        <NavLink to='/collections' onClick={onClose}>Collections</NavLink>
+                        <NavLink
+                            to='/collections'
+                            className={({ isActive }) =>
+                                isActive
+                                    ? "drawer-navigation-link current"
+                                    : "drawer-navigation-link"
+                            }
+                            onClick={onClose}
+                        >
+                            Collections
+                        </NavLink>
                     </li>
 
                     <li className='drawer-navigation-link'>
-                        <NavLink to='/about' onClick={onClose}>About</NavLink>
+                        <NavLink
+                            to='/about'
+                            className={({ isActive }) =>
+                                isActive
+                                    ? "drawer-navigation-link current"
+                                    : "drawer-navigation-link"
+                            }
+                            onClick={onClose}
+                        >
+                            About
+                        </NavLink>
                     </li>
                 </ul>
             </div>
