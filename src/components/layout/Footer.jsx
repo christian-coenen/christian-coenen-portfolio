ee577632a5e4dd4bb9d936352f46b001157f3749
Christian Coenen <christian300507@yahoo.com>
2026-03-11 15:52:13 +0100

    layout(footer): add footer with copyright and social links
    - Add footer component to portfolio layout.
    - Include copyright notice.
    - Add external links to GitHub and LinkedIn.
    - Implement basic hover states for links.

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,8 +1,74 @@
+import './Footer.css'
+
 const Footer = () => {
     return (
-        <div>
+        <footer className='footer'>
+            <div className='footer-inner'>
+                <div className='footer-copyright'>
+                    <span className='footer-copyright-text'>Copyright © Christian Coenen, 2026</span>
+                </div>
+                <div className='footer-credits'>
+                    <span className='footer-credits-text'>Gemaakt door Christian Coenen</span>
 
-        </div>
+                    <ul className='footer-links'>
+                        <li className='footer-link-item'>
+                            <a
+                                href='https://www.linkedin.com/in/christian-coenen-cc'
+                                title='LinkedIn'
+                                target='_blank'
+                                rel='noopener noreferrer'
+                            >
+                                LinkedIn
+                            </a>
+                        </li>
+
+                        <li className='footer-link-item'>
+                            <a
+                                href='https://github.com/christian-coenen'
+                                title='GitHub'
+                                target='_blank'
+                                rel='noopener noreferrer'
+                            >
+                                GitHub
+                            </a>
+                        </li>
+
+                        <li className='footer-link-item'>
+                            <a
+                                href='https://github.com/christian-coenen'
+                                title='GitHub'
+                                target='_blank'
+                                rel='noopener noreferrer'
+                            >
+                                GitHub
+                            </a>
+                        </li>
+
+                        <li className='footer-link-item'>
+                            <a
+                                href='https://github.com/christian-coenen'
+                                title='GitHub'
+                                target='_blank'
+                                rel='noopener noreferrer'
+                            >
+                                GitHub
+                            </a>
+                        </li>
+
+                        <li className='footer-link-item'>
+                            <a
+                                href='https://github.com/christian-coenen'
+                                title='GitHub'
+                                target='_blank'
+                                rel='noopener noreferrer'
+                            >
+                                GitHub
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </footer>
     )
 }
 
