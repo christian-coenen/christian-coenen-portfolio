e7215d6eab4bb0b546b2ef7882d146efb6c2eb9f
Christian Coenen <christian300507@yahoo.com>
2026-03-18 15:46:34 +0100

    feat(projects-intro-section): add intro section to projects page
    - Add intro section to projects page.

--- src/pages/ProjectsPage.jsx
+++ src/pages/ProjectsPage.jsx
@@ -1,9 +1,10 @@
 import PortfolioLayout from '../components/layout/PortfolioLayout'
+import ProjectsIntroSection from '../components/page-sections/projects/ProjectsIntroSection'
 
 const ProjectPage = () => {
     return (
         <PortfolioLayout>
-
+            <ProjectsIntroSection />
         </PortfolioLayout>
     )
 }
