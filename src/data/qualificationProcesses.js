8a0b543484faed2b9bab95c9dc657c8e31b2e9df
Christian Coenen <christian300507@yahoo.com>
2026-04-17 23:09:40 +0200

    feat(qualification-detail-page): add descriptions for work processes B1-K2
    - Add descriptions of B1-K2 work processes.
    - Fix qualification B1-K1 work processes cards not appearing.

--- src/data/qualificationProcesses.js
+++ src/data/qualificationProcesses.js
@@ -2,7 +2,7 @@ export const qualificationProcesses = {
     "b1-k1-w1": {
         title: "B1-K1-W1: Plant werkzaamheden en bewaakt de voortgang",
         coreTask: "b1-k1",
-        description: `De beginnend beroepsbeoefenaar stemt met opdrachtgever en betrokkenen af wat er ontwikkeld moet worden en binnen welke termijn. Hij/zij verzamelt eisen, wensen en specificaties en vertaalt deze naar concrete doelen en een haalbare planning. Tijdens het ontwikkelproces bewaakt hij/zij de voortgang, houdt taken bij en communiceert tijdig over risico’s of afwijkingen in planning en doelen.`,
+        description: `De beginnend beroepsbeoefenaar stemt met opdrachtgever en betrokkenen af wat er ontwikkeld moet worden en binnen welke termijn. Hij/zij verzamelt eisen, wensen en specificaties en vertaalt deze naar concrete doelen en een haalbare planning. Tijdens het ontwikkelproces bewaakt hij/zij de voortgang, houdt taken bij en communiceert tijdig over risicos of afwijkingen in planning en doelen.`,
         evidence: [
             {
                 title: "Tekstdocument plaatshouder",
@@ -14,8 +14,8 @@ export const qualificationProcesses = {
 
     "b1-k1-w2": {
         title: "B1-K1-W2: Ontwerpt software",
-        coreTask: `De beginnend beroepsbeoefenaar maakt een (deel)ontwerp dat aansluit op de gestelde eisen en wensen. Hij/zij vergelijkt mogelijke oplossingen en werkt het meest geschikte ontwerp uit op basis van de opdracht en planning. Daarbij houdt hij/zij rekening met de ontwikkelomgeving, gebruikte technologieën en eisen rond privacy, ethiek en veiligheid.`,
-        description: "Beschrijving B1-K1-W2.",
+        coreTask: "b1-k1",
+        description: `De beginnend beroepsbeoefenaar maakt een (deel)ontwerp dat aansluit op de gestelde eisen en wensen. Hij/zij vergelijkt mogelijke oplossingen en werkt het meest geschikte ontwerp uit op basis van de opdracht en planning. Daarbij houdt hij/zij rekening met de ontwikkelomgeving, gebruikte technologieën en eisen rond privacy, ethiek en veiligheid.`,
         evidence: [
 
         ]
@@ -23,8 +23,8 @@ export const qualificationProcesses = {
 
     "b1-k1-w3": {
         title: "B1-K1-W3: Realiseert (onderdelen van) software",
-        coreTask: `De beginnend beroepsbeoefenaar realiseert (onderdelen van) software op basis van het (deel)ontwerp. Hij/zij schrijft en integreert code en benodigde assets en zorgt voor een logische en efficiënte structuur. Daarbij controleert hij/zij of de software voldoet aan geldende codeconventies en eisen rond privacy en veiligheid.`,
-        description: "Beschrijving B1-K1-W3.",
+        coreTask: "b1-k1",
+        description: `De beginnend beroepsbeoefenaar realiseert (onderdelen van) software op basis van het (deel)ontwerp. Hij/zij schrijft en integreert code en benodigde assets en zorgt voor een logische en efficiënte structuur. Daarbij controleert hij/zij of de software voldoet aan geldende codeconventies en eisen rond privacy en veiligheid.`,
         evidence: [
 
         ]
@@ -32,8 +32,8 @@ export const qualificationProcesses = {
 
     "b1-k1-w4": {
         title: "B1-K1-W4: Test software",
-        coreTask: `De beginnend beroepsbeoefenaar stelt testscenario’s op en kiest een passende testvorm en methodiek voor de software. Hij/zij bepaalt de benodigde testomgeving, middelen en testdata en voert de tests uit. Vervolgens analyseert hij/zij de resultaten en trekt conclusies op basis van de bevindingen.`,
-        description: "Beschrijving B1-K1-W4.",
+        coreTask: "b1-k1",
+        description: `De beginnend beroepsbeoefenaar stelt testscenario’s op en kiest een passende testvorm en methodiek voor de software. Hij/zij bepaalt de benodigde testomgeving, middelen en testdata en voert de tests uit. Vervolgens analyseert hij/zij de resultaten en trekt conclusies op basis van de bevindingen.`,
         evidence: [
 
         ]
@@ -41,8 +41,8 @@ export const qualificationProcesses = {
 
     "b1-k1-w5": {
         title: "B1-K1-W5: Doet verbetervoorstellen voor de software",
-        coreTask: `De beginnend beroepsbeoefenaar analyseert wensen, feedback, testresultaten en meldingen van opdrachtgever of belanghebbenden met betrekking tot software. Hij/zij vertaalt deze input naar concrete voorstellen voor verbetering van de software. Daarbij stemt hij/zij af welke aanpassingen nodig zijn en binnen welke planning deze worden uitgevoerd.`,
-        description: "Beschrijving B1-K1-W5.",
+        coreTask: "b1-k1",
+        description: `De beginnend beroepsbeoefenaar analyseert wensen, feedback, testresultaten en meldingen van opdrachtgever of belanghebbenden met betrekking tot software. Hij/zij vertaalt deze input naar concrete voorstellen voor verbetering van de software. Daarbij stemt hij/zij af welke aanpassingen nodig zijn en binnen welke planning deze worden uitgevoerd.`,
         evidence: [
 
         ]
@@ -51,7 +51,7 @@ export const qualificationProcesses = {
     "b1-k2-w1": {
         title: "B1-K2-W1: Voert overleg",
         coreTask: "b1-k2",
-        description: "Beschrijving B1-K2-W1.",
+        description: `De beginnend beroepsbeoefenaar voert overleg met de leidinggevende en/of het projectteam over de voortgang van werkzaamheden. Hij/zij informeert over eigen taken en resultaten en brengt ideeën, oplossingen en knelpunten in. Daarnaast vraagt hij/zij waar nodig advies en maakt afspraken over vervolgstappen.`,
         evidence: [
 
         ]
@@ -60,7 +60,7 @@ export const qualificationProcesses = {
     "b1-k2-w2": {
         title: "B1-K2-W2: Presenteert het opgeleverde werk",
         coreTask: "b1-k2",
-        description: "Beschrijving B1-K2-W2.",
+        description: `De beginnend beroepsbeoefenaar presenteert het opgeleverde (deel)product aan opdrachtgever, leidinggevende of team. Hij/zij licht de functionaliteiten en uitgevoerde werkzaamheden toe en toont resultaten zoals ontwerpen, software of testresultaten. Daarnaast beantwoordt hij/zij vragen en legt gemaakte keuzes uit.`,
         evidence: [
 
         ]
@@ -69,7 +69,7 @@ export const qualificationProcesses = {
     "b1-k2-w3": {
         title: "B1-K2-W3: Reflecteert op het werk",
         coreTask: "b1-k2",
-        description: "Beschrijving B1-K2-W3.",
+        description: `De beginnend beroepsbeoefenaar neemt deel aan de evaluatie na oplevering van een (deel)product. Hij/zij reflecteert op eigen prestaties en het teamproces of de werkwijze. Daarnaast benoemt hij/zij verbeterpunten en geeft indien nodig feedback aan collega’s.`,
         evidence: [
 
         ]
