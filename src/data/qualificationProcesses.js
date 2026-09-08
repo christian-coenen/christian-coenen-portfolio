494f711c8f32bfe0ab1edacd5ac6582184abee0b
Christian Coenen <christian300507@yahoo.com>
2026-09-08 14:55:02 +0200

    feat(qualificationProcesses): add media data to qualification processes page
    - Add data to qualification processes page.

--- src/data/qualificationProcesses.js
+++ src/data/qualificationProcesses.js
@@ -5,9 +5,46 @@ export const qualificationProcesses = {
         description: `De beginnend beroepsbeoefenaar stemt met opdrachtgever en betrokkenen af wat er ontwikkeld moet worden en binnen welke termijn. Hij/zij verzamelt eisen, wensen en specificaties en vertaalt deze naar concrete doelen en een haalbare planning. Tijdens het ontwikkelproces bewaakt hij/zij de voortgang, houdt taken bij en communiceert tijdig over risicos of afwijkingen in planning en doelen.`,
         evidence: [
             {
-                title: "Tekstdocument plaatshouder",
-                link: "/evidence/evidence-placeholder.txt",
-                type: "default"
+                title: "Behoefteanalyse - De Recycle Fabriek 1.2.1",
+                mediaType: "documents",
+                file: "Behoefteanalyse - De Recycle Fabriek 1.2.1.pdf",
+                type: "document",
+            },
+            {
+                title: "Behoefteanalyse LABOR Arbeidsintegratie Automatisering",
+                mediaType: "documents",
+                file: "Behoefteanalyse LABOR Arbeidsintegratie Automatisering.pdf",
+                type: "document",
+            },
+            {
+                title: "Dagelijkse stand-up Collabotics 24 juni",
+                mediaType: "videos",
+                file: "Dagelijkse stand-up Collabotics 24 juni.mp4",
+                type: "video",
+            },
+            {
+                title: "Dagelijkse stand-up Collabotics 25 juni",
+                mediaType: "videos",
+                file: "Dagelijkse stand-up Collabotics 25 juni.mp4",
+                type: "video",
+            },
+            {
+                title: "Toelichting bij BPV eindgesprek C. Coenen - bewaken voortgang",
+                mediaType: "documents",
+                file: "Toelichting bij BPV eindgesprek C. Coenen - bewaken voortgang.pdf",
+                type: "document",
+            },
+            {
+                title: "Werken in een ontwikkelteam met dagelijkse stand-ups",
+                mediaType: "documents",
+                file: "Werken in een ontwikkelteam met dagelijkse stand-ups.pdf",
+                type: "document",
+            },
+            {
+                title: "YouTrack Gantt chart planning",
+                mediaType: "documents",
+                file: "YouTrack Gantt chart planning.pdf",
+                type: "document",
             },
         ]
     },
@@ -17,7 +54,24 @@ export const qualificationProcesses = {
         coreTask: "b1-k1",
         description: `De beginnend beroepsbeoefenaar maakt een (deel)ontwerp dat aansluit op de gestelde eisen en wensen. Hij/zij vergelijkt mogelijke oplossingen en werkt het meest geschikte ontwerp uit op basis van de opdracht en planning. Daarbij houdt hij/zij rekening met de ontwikkelomgeving, gebruikte technologieën en eisen rond privacy, ethiek en veiligheid.`,
         evidence: [
-
+            {
+                title: "Flowchart test website Internet.nl",
+                mediaType: "documents",
+                file: "Flowchart test website Internet.nl.pdf",
+                type: "document",
+            },
+            {
+                title: "Functioneel ontwerp Collabotics website",
+                mediaType: "documents",
+                file: "Functioneel ontwerp Collabotics website.pdf",
+                type: "document",
+            },
+            {
+                title: "Technisch ontwerp Collabotics website",
+                mediaType: "documents",
+                file: "Technisch ontwerp Collabotics website.pdf",
+                type: "document",
+            },
         ]
     },
 
@@ -26,7 +80,18 @@ export const qualificationProcesses = {
         coreTask: "b1-k1",
         description: `De beginnend beroepsbeoefenaar realiseert (onderdelen van) software op basis van het (deel)ontwerp. Hij/zij schrijft en integreert code en benodigde assets en zorgt voor een logische en efficiënte structuur. Daarbij controleert hij/zij of de software voldoet aan geldende codeconventies en eisen rond privacy en veiligheid.`,
         evidence: [
-
+            {
+                title: "Codeconventies Collabotics website",
+                mediaType: "documents",
+                file: "Codeconventies Collabotics website.pdf",
+                type: "document",
+            },
+            {
+                title: "Versiebeheer GitHub Christian Coenen portfoliowebsite",
+                mediaType: "images",
+                file: "Versiebeheer GitHub Christian Coenen portfoliowebsite.png",
+                type: "image",
+            },
         ]
     },
 
@@ -35,7 +100,18 @@ export const qualificationProcesses = {
         coreTask: "b1-k1",
         description: `De beginnend beroepsbeoefenaar stelt testscenario’s op en kiest een passende testvorm en methodiek voor de software. Hij/zij bepaalt de benodigde testomgeving, middelen en testdata en voert de tests uit. Vervolgens analyseert hij/zij de resultaten en trekt conclusies op basis van de bevindingen.`,
         evidence: [
-
+            {
+                title: "Testplan Pro-Light pdf-automatisering",
+                mediaType: "documents",
+                file: "Testplan Pro-Light pdf-automatisering.pdf",
+                type: "image",
+            },
+            {
+                title: "Testrapport Pro-Light pdf-automatisering",
+                mediaType: "documents",
+                file: "Testrapport Pro-Light pdf-automatisering.pdf",
+                type: "document",
+            },
         ]
     },
 
@@ -44,7 +120,18 @@ export const qualificationProcesses = {
         coreTask: "b1-k1",
         description: `De beginnend beroepsbeoefenaar analyseert wensen, feedback, testresultaten en meldingen van opdrachtgever of belanghebbenden met betrekking tot software. Hij/zij vertaalt deze input naar concrete voorstellen voor verbetering van de software. Daarbij stemt hij/zij af welke aanpassingen nodig zijn en binnen welke planning deze worden uitgevoerd.`,
         evidence: [
-
+            {
+                title: "Verslag verbetervoorstellen Collabotics website",
+                mediaType: "documents",
+                file: "Verslag verbetervoorstellen Collabotics website.pdf",
+                type: "document",
+            },
+            {
+                title: "YouTrack Gantt chart planning",
+                mediaType: "documents",
+                file: "YouTrack Gantt chart planning.pdf",
+                type: "document",
+            },
         ]
     },
 
@@ -53,7 +140,36 @@ export const qualificationProcesses = {
         coreTask: "b1-k2",
         description: `De beginnend beroepsbeoefenaar voert overleg met de leidinggevende en/of het projectteam over de voortgang van werkzaamheden. Hij/zij informeert over eigen taken en resultaten en brengt ideeën, oplossingen en knelpunten in. Daarnaast vraagt hij/zij waar nodig advies en maakt afspraken over vervolgstappen.`,
         evidence: [
-
+            {
+                title: "Codeconventies Collabotics website",
+                mediaType: "documents",
+                file: "Codeconventies Collabotics website.pdf",
+                type: "document",
+            },
+            {
+                title: "Dagelijkse stand-up Collabotics 24 juni",
+                mediaType: "videos",
+                file: "Dagelijkse stand-up Collabotics 24 juni.mp4",
+                type: "video",
+            },
+            {
+                title: "Dagelijkse stand-up Collabotics 25 juni",
+                mediaType: "videos",
+                file: "Dagelijkse stand-up Collabotics 25 juni.mp4",
+                type: "video",
+            },
+            {
+                title: "Toelichting bij BPV eindgesprek C. Coenen - bewaken voortgang",
+                mediaType: "documents",
+                file: "Toelichting bij BPV eindgesprek C. Coenen - bewaken voortgang.pdf",
+                type: "document",
+            },
+            {
+                title: "Werken in een ontwikkelteam met dagelijkse stand-ups",
+                mediaType: "documents",
+                file: "Werken in een ontwikkelteam met dagelijkse stand-ups.pdf",
+                type: "document",
+            },
         ]
     },
 
@@ -62,7 +178,30 @@ export const qualificationProcesses = {
         coreTask: "b1-k2",
         description: `De beginnend beroepsbeoefenaar presenteert het opgeleverde (deel)product aan opdrachtgever, leidinggevende of team. Hij/zij licht de functionaliteiten en uitgevoerde werkzaamheden toe en toont resultaten zoals ontwerpen, software of testresultaten. Daarnaast beantwoordt hij/zij vragen en legt gemaakte keuzes uit.`,
         evidence: [
-
+            {
+                title: "Presentatie Collabotics intern-project (deel 1 van 2)",
+                mediaType: "videos",
+                file: "Presentatie Collabotics intern-project (deel 1 van 2).mp4",
+                type: "video",
+            },
+            {
+                title: "Presentatie Collabotics intern-project (deel 2 van 2)",
+                mediaType: "videos",
+                file: "Presentatie Collabotics intern-project (deel 2 van 2).mp4",
+                type: "video",
+            },
+            {
+                title: "Presentatie Collabotics website",
+                mediaType: "videos",
+                file: "Presentatie Collabotics website.mp4",
+                type: "video",
+            },
+            {
+                title: "Verslag presentatie Internet.nl-project",
+                mediaType: "documents",
+                file: "Verslag presentatie Internet.nl-project.pdf",
+                type: "document",
+            },
         ]
     },
 
@@ -71,7 +210,12 @@ export const qualificationProcesses = {
         coreTask: "b1-k2",
         description: `De beginnend beroepsbeoefenaar neemt deel aan de evaluatie na oplevering van een (deel)product. Hij/zij reflecteert op eigen prestaties en het teamproces of de werkwijze. Daarnaast benoemt hij/zij verbeterpunten en geeft indien nodig feedback aan collega’s.`,
         evidence: [
-
+            {
+                title: "Reflectieverslag van mijn stage bij Collabotics",
+                mediaType: "documents",
+                file: "Reflectieverslag van mijn stage bij Collabotics.pdf",
+                type: "document",
+            }
         ]
     }
 }
